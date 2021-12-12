1f996de8b268090b6aec2a65e4610e875622c92c
David Pratt <david@midnightmechanism.com>
2021-12-11 23:19:35 -0600

    Connect to remote db

--- federal-data/app.R
+++ federal-data/app.R
@@ -1,69 +1,51 @@
-library(pipeR)
-library(DT)
-library(FredR)
-library(plotly)
+## app.R ##
+library(shinydashboard)
 library(shiny)
-library(dplyr)
+library(purrr)
+library(ggplot2)
+library(fredr)
+library(RPostgres)
 
-source(".api_key")
-api_key = key
+source(".pg_user")
+source(".pg_pwd")
+source(".db_name")
+source(".pg_host")
+source(".pg_db")
 
-fred <- FredR(api_key)
-fed_rev <- fred$series.observations(series_id = 'FYFRGDA188S') # as a fraction of GDP
-top_marg <- fred$series.observations(series_id = 'IITTRHB') # Top marginal rate
-fed_rev$date <- as.Date(fed_rev$date)
-top_marg$date <- as.Date(top_marg$date)
-rev_marg <- inner_join(fed_rev, top_marg, by = "date", keep = F)
-rev_marg <- as.data.frame(rev_marg)
-rev_marg <- cbind.data.frame(rev_marg$date, rev_marg$value.x, rev_marg$value.y)
-colnames(rev_marg) = c("date", "gdp_rev", "top_marg")
+con <- dbConnect(RPostgres::Postgres(), 
+                 user = pg_user, 
+                 password = pg_pwd,
+                 host = pg_host,
+                 dbname = pg_db)
 
-# consider only the annual revenue for the histogram
-annual_rev = as.numeric(matrix(rev_marg$gdp_rev))
+series_list = dbGetQuery(con, "SELECT series FROM table_descriptions")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Federal Revenue Statistics"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      numericInput("yrs", 
-                   "Last N Years:", 
-                   value = 75, 
-                   min = 1, 
-                   length(annual_rev))),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot1", width = 600, height = 400),
-      plotOutput("distPlot2", width = 600, height = 400)
+ui <- dashboardPage(
+  dashboardHeader(title = "FRED Data Browser"),
+  dashboardSidebar(
+    selectInput("select1", "Select a series", 
+                choices = series_list, selected = tolower("FYFRGDA188S")),
+    selectInput("select2", "Select a series", 
+                choices = series_list, selected = tolower("IITTRHB"))
+  ),
+  dashboardBody(
+    fluidRow(
+      box(plotOutput("plot1", height = 250)
+      )
     )
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$distPlot1 <- renderPlot({
-    hist(tail(annual_rev, input$yrs),
-         main = "Revenue as a Percentage of GDP",
-         xlab = "Percentage of GDP",
-         col = "goldenrod")
-  })
-  output$distPlot2 <- renderPlot({
-    data = tail(rev_marg, input$yrs) # subset data
-    plot(as.numeric(data[ ,2]), 
-         as.numeric(data[ ,3]),
-         type = "p",
-         main = "Revenue Percent vs. Top Marginal Rate",
-         xlab = "Top Marginal Rate",
-         ylab = "Revenue Percentage of GDP",
-         col = "forestgreen") + grid()
+  output$plot1 <- renderPlot({
+    series1 = dbGetQuery(con, paste("SELECT date, value, series_id FROM ", input$select1, ";")) # gdpca 
+    series2 = dbGetQuery(con, paste("SELECT date, value, Series_id FROM ", input$select2, ";")) # gdp
+    rbind.data.frame(series1, series2) %>%
+      ggplot(data = ., mapping = aes(x = date, y = value, color = series_id)) +
+      geom_line() +
+      labs(x = "Observation Date", y = "Rate", color = "Series")
   })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
 
